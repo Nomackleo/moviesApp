69ce13b9eb99432785664eed91b0a4fa65f74e99
Nomackleo <nomack3d@gmail.com>
2020-08-25 10:36:32 -0500

    Version SSH

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: '*', pathMatch: 'full', redirectTo: 'home' }
+  { path: '*', pathMatch: 'full', redirectTo: 'home' },
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
 ];
 
 @NgModule({
--- src/app/services/pelis.service.ts
+++ src/app/services/pelis.service.ts
@@ -11,10 +11,15 @@ export class PelisService {
 
   private apiKey:string = "6b887e2c608d46c2fd3d820ece7fe4b7"
   private urlMoviedb:string = "https://api.themoviedb.org/3"
+   
   constructor( private jsp:HttpClientJsonpModule,
                private http:HttpClient ) { }
 
   
+  getCurrent() {
+    let url = `${ this.urlMoviedb }//discover/movie?primary_release_date.gte=2014-09-15&primary_release_date.lte=2014-10-22&api_key=${ this.apiKey }&callback=test`
+    return this.http.jsonp( url, 'callback' ).pipe(map( res => res ))
+  }             
   getFavorites() {
     let url = `${ this.urlMoviedb }/discover/movie?sort_by=popularity.desc&api_key=${ this.apiKey }&callback=test`
     return this.http.jsonp( url, 'callback' ).pipe(map( res => res ))
